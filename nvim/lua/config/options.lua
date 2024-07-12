c320eecf08447a408926d62f73aacc502f71875a
Encounter005 <1624221574@qq.com>
2024-07-12 16:49:36 +0800

    MODIFY: change the default colorscheme

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,5 +1,5 @@
 -- Set colorscheme
-local colorscheme = "catppuccin-mocha"
+local colorscheme = "tokyonight"
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
     vim.notify("colorscheme " .. colorscheme .. " not found!")
