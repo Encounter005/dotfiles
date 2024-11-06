f355132e87d862a0a004367cd479a8506d99f29f
Encounter005 <110457617+Encounter005@users.noreply.github.com>
2024-11-06 21:21:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# packages
+yay -S yayfzf nvidia-dkms nvidia-utils hyprland waybar xdg-desktop-portal-hyprland libinput-gestures downgrade\
+neovim pipewire  pipewire-alsa pipewire-jack  pipewire-audio pipewire-pulse wireplumber pavucontrol pamixer networkmanager\
+network-manager-applet bluez bluez-utils blueman brightnessctl udiskie sddm qt5-quickcontrols qt5-quickcontrols2\
+qt5-graphicaleffects  rofi-wayland  swww swaylock-effects-git wlogout grimblast-git swappy cliphist slurp polkit-gnome \
+xdg-desktop-portal-gtk  pacman-contrib parallel jq imagemagick deno nemo libnotify nwg-drawer qt5ct qt6ct kvantum kvantum-qt5 \
+qt5-wayland qt6-wayland  kitty ark vim code yazi zsh oh-my-zsh-git neofetch 
+
+
 # Define the source and target directories
 source_dir=$(pwd)
 target_dir="$HOME/.config"
