7a8fea9a2ed84b99674fa7192bdb0eb210397307
encounter005 <1624221574@qq.com>
2025-12-26 18:25:50 +0800

    移除conda的支持

--- doom/init.el
+++ doom/init.el
@@ -154,12 +154,12 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam2 +noter +pomodoro +pretty +pandoc +hugo +journal +dragndrop)               ; organize your plain life in plain text
+       (org +roam2 +noter +pomodoro +pretty +pandoc +hugo +journal +dragndrop) ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
-       ( python +lsp +tree-sitter +pyright +cython +conda)            ; beautiful is better than ugly
+       ( python +lsp +tree-sitter +pyright +cython +pyenv)            ; beautiful is better than ugly
        qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
